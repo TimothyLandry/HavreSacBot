9facc550e1b875ea5902b8e5c52ce317b55334f5
Timothy Landry <31513409+TimothyLandry@users.noreply.github.com>
2023-03-18 16:13:23 -0400

    Forestier

--- bot.py
+++ bot.py
@@ -4,32 +4,36 @@ from logic.captchaLogic import CaptchaLogic
 
 DEBUG = True
 HARVEST_TYPE = "SEED" # "RESOURCE" "SEED"
+JOB = "forestier"
+RESOURCE_NAME = "sureau"
 KEYBIND = "3"
 SHIFT = True
 CTRL = False
+
+
 
 p = ["croton","./patterns/herboriste/croton.png", 0.96, (255,255,255)]
 # pazinclou
 
 # Init Logics
-harvestLogic = HarvestLogic(DEBUG, HARVEST_TYPE, KEYBIND, SHIFT, CTRL)
-templateLogic = TemplateLogic(DEBUG, "./patterns/herboriste/croton.png", 0.95)
+harvestLogic = HarvestLogic(DEBUG, JOB, HARVEST_TYPE, KEYBIND, SHIFT, CTRL)
+templateLogic = TemplateLogic(DEBUG, f"./patterns/{JOB}/{RESOURCE_NAME}.png", 0.95)
 captchaLogic = CaptchaLogic(DEBUG)
 
 # Coordinates for 400 x 350 around character
-x1=750
-y1=350
-x2=1150
-y2=700
+x1=600 #750
+y1=150 #350
+x2=1300 #1150
+y2=800 #700
 
 #######################
 # Loop
 #######################
 while True:
     if(captchaLogic.checkForFight()):
-    #if(True):
         print("\nCAPCHAT FOUND\n")
-        captchaLogic.solve()
+        break
+        #captchaLogic.solve()
         
 
     # Template Logic
--- logic/harvestLogic.py
+++ logic/harvestLogic.py
@@ -4,14 +4,25 @@ from logic.keyLogic import keyDown, keyUp, keyPress
 from logic.templateLogic import TemplateLogic
 
 class HarvestLogic:
-    def __init__(self, DEBUG, HARVEST_TYPE, KEYBIND, SHIFT, CTRL):
+    def __init__(self, DEBUG, JOB, HARVEST_TYPE, KEYBIND, SHIFT, CTRL):
+        self.JOB = JOB
+        self.HARVEST_TYPE = HARVEST_TYPE
         self.KEYBIND = KEYBIND
         self.SHIFT = SHIFT
         self.CTRL = CTRL
-        self.HARVEST_TYPE = HARVEST_TYPE
 
-        self.cutTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/resource1.png", 0.9)
-        self.seedTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/seed.png", 0.9)
+        if(JOB == "herboriste"):
+            self.cutTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/cut_herboriste.png", 0.9)
+            self.seedTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/cut_herboriste.png", 0.9)
+            self.cutTime = 3
+            self.seedTime = 3
+        elif(JOB == "forestier"):
+            self.cutTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/cut_forestier.png", 0.9)
+            self.seedTemplateLogic = TemplateLogic(DEBUG, "./patterns/harvest/seed_forestier.png", 0.9)
+            self.cutTime = 5
+            self.seedTime = 5
+
+        
 
     def interact(self, point):
         rightClick(point)
@@ -21,7 +32,7 @@ class HarvestLogic:
         p = self.cutTemplateLogic.getNearestTemplate(point[0]-100, point[1]-150, point[0]+100, point[1]+50)
         if(p):
             leftClick(p)
-            sleep(3)
+            sleep(self.cutTime)
 
     def seed(self, point):
         p = self.seedTemplateLogic.getNearestTemplate(point[0]-100, point[1]-150, point[0]+100, point[1]+50)
@@ -29,7 +40,7 @@ class HarvestLogic:
             self.cut(point)
         else:
             leftClick(p)
-            sleep(3)
+            sleep(self.seedTime)
         return
 
     def replant(self, point):
@@ -60,7 +71,6 @@ class HarvestLogic:
 
     def harvest(self, point):
         self.interact(point)
-        
         if(self.HARVEST_TYPE == "SEED"):
             self.seed(point)
         if(self.HARVEST_TYPE == "RESOURCE"):
